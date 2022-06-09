2876d0e2b258f1dfa1d01fae83761a41c38544ca
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-06-09 13:09:38 +0100

    feat(06): display error msg on condition

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,19 +5,34 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef()
+  const [errorMessage, setErrorMessage] = React.useState(null)
 
   function handleSubmit(event) {
     event.preventDefault()
     onSubmitUsername(inputRef.current.value)
   }
 
+  function handleChange(event) {
+    const isInputValid = event.target.value === event.target.value.toLowerCase()
+
+    setErrorMessage(isInputValid ? null : 'Nop nop nop!')
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input ref={inputRef} id="username" type="text" />
+        <input
+          ref={inputRef}
+          id="username"
+          type="text"
+          onChange={handleChange}
+        />
       </div>
-      <button type="submit">Submit</button>
+      <div style={{color: 'red'}}>{errorMessage}</div>
+      <button type="submit" disabled={!!errorMessage}>
+        Submit
+      </button>
     </form>
   )
 }
